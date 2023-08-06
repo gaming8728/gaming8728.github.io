3ab9813c7e4d6007b0b818a3a3dde699e864b336
gaming8728 <93265111+gaming8728@users.noreply.github.com>
2023-08-06 15:55:20 +0800

    ok

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ function togglePopup() {
 
 
 
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('transparent');
+    } else {
+      navbar.classList.remove('transparent');
+    }
+  });
+  
+
 
