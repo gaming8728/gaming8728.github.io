b1b8156060ab2f7da7fba7d39ddd280deb16fd3e
gaming8728 <93265111+gaming8728@users.noreply.github.com>
2023-08-08 00:11:22 +0800

    ok

--- script.js
+++ script.js
@@ -22,4 +22,24 @@ function togglePopup() {
   
 
 
+  function openTab(evt, tabName) {
+    var i, tabContent, tabButton;
+    
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    
+    tabButton = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButton.length; i++) {
+      tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
   
+  window.onload = function() {
+    // 預設顯示分頁一
+    document.getElementById("home").style.display = "block";
+  };
