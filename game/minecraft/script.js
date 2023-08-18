8c2d3f34619282ed7bb47e7c875c840bc2ed9d3e
gaming8728 <93265111+gaming8728@users.noreply.github.com>
2023-08-18 15:51:28 +0800

    ok

---
+++ game/minecraft/script.js
@@ -0,0 +1,57 @@
+function togglePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = (popup.style.display === "none") ? "block" : "none";
+  }
+  
+  // 頁面加載後自動關閉彈出框
+  window.onload = function() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
+
+
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('transparent');
+    } else {
+      navbar.classList.remove('transparent');
+    }
+  });
+  
+
+
+  function openTab(evt, tabName) {
+    var i, tabContent, tabButton;
+    
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    
+    tabButton = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButton.length; i++) {
+      tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  window.onload = function() {
+    // 預設顯示分頁一
+    document.getElementById("home").style.display = "block";
+    };
+
+
+  window.addEventListener('wheel', function(event) {
+    var element = document.getElementById('myElement');
+    var rect = element.getBoundingClientRect();
+  
+    if (event.deltaY < 0) {
+      element.classList.add('transparent');
+    } else {
+      element.classList.remove('transparent');
+    }
+  });
--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-function togglePopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = (popup.style.display === "none") ? "block" : "none";
+function openTab(evt, tabName) {
+  var i, tabContent, tabButton;
+  
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
   }
   
-  // 頁面加載後自動關閉彈出框
-  window.onload = function() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "none";
-  }
-
-
-
-  window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 0) {
-      navbar.classList.add('transparent');
-    } else {
-      navbar.classList.remove('transparent');
-    }
-  });
-  
-
-
-  function openTab(evt, tabName) {
-    var i, tabContent, tabButton;
-    
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    
-    tabButton = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabButton.length; i++) {
-      tabButton[i].className = tabButton[i].className.replace(" active", "");
-    }
-    
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  tabButton = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButton.length; i++) {
+    tabButton[i].className = tabButton[i].className.replace(" active", "");
   }
   
-  window.onload = function() {
-    // 預設顯示分頁一
-    document.getElementById("home").style.display = "block";
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+window.onload = function() {
+  // 預設顯示分頁一
+  document.getElementById("home").style.display = "block";
   };
 
 
-  window.addEventListener('wheel', function(event) {
-    var element = document.getElementById('myElement');
-    var rect = element.getBoundingClientRect();
-  
-    if (event.deltaY < 0) {
-      element.classList.add('transparent');
-    } else {
-      element.classList.remove('transparent');
-    }
-  });
+window.addEventListener('wheel', function(event) {
+  var element = document.getElementById('myElement');
+  var rect = element.getBoundingClientRect();
+
+  if (event.deltaY < 0) {
+    element.classList.add('transparent');
+  } else {
+    element.classList.remove('transparent');
+  }
+});
+
+
+
+window.location.href = "game/minecraft/index.html";
